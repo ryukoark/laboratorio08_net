8b50431fc9189e2ded39bfe7bc30c673c5626148
robbiejude murillo Gonza <168230819+ryukoark@users.noreply.github.com>
2025-10-09 10:37:36 -0500

    creacion de scafold

---
+++ Lab08Robbiejude/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab08Robbiejude.Models;
+
+public partial class Client
+{
+    public int Clientid { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
---
+++ Lab08Robbiejude/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab08Robbiejude.Models;
+
+public partial class Order
+{
+    public int Orderid { get; set; }
+
+    public int Clientid { get; set; }
+
+    public DateTime Orderdate { get; set; }
+
+    public virtual Client Client { get; set; } = null!;
+
+    public virtual ICollection<Orderdetail> Orderdetails { get; set; } = new List<Orderdetail>();
+}
